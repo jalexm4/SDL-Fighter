3e9ddc609fa4c95cbf7fa1245e41448323634df3
James <jamesmalcolm444@gmail.com>
2024-05-18 13:53:53 +0100

    Player movement added

--- main.c
+++ main.c
@@ -11,8 +11,8 @@
 typedef struct
 {
     // Game Config
-    unsigned int window_width;
-    unsigned int window_height;
+    int window_width;
+    int window_height;
 
     // Window
     SDL_Window *window;
@@ -26,15 +26,27 @@ typedef struct
 typedef struct
 {
     bool quit;
+
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+
+    bool fire;
 } Keys;
 
 typedef struct
 {
     int x, y, w, h;
+    int x_velocity;
+    int y_velocity;
+    int speed;
+
     SDL_Texture *texture;
 } Player;
 
 
+void process_input(Keys *keys, Player *player);
 void handle_keyboard(Keys *keys);
 
 
@@ -82,19 +94,22 @@ int main(void)
     }
 
     // Game Init
-    Keys keys = {false};
+    Keys keys = {false, false, false, false, false, false};
 
     Player player;
     player.texture = IMG_LoadTexture(game.renderer, "assets/player.png");
     player.x = 100;
     player.y = 100;
     SDL_QueryTexture(player.texture, NULL, NULL, &player.w, &player.h);
+    player.x_velocity = 0;
+    player.y_velocity = 0;
+    player.speed = 500;
 
     // Game Loop
     while (1)
     {
         // Deltatime for current frame
-        game.delta_time = (SDL_GetTicks() - game.last_frame_time) / 1000.0;
+        game.delta_time = (SDL_GetTicks() - game.last_frame_time) / 1000.0f;
         game.last_frame_time = SDL_GetTicks();
 
         // Respond to users keyboard input
@@ -104,6 +119,32 @@ int main(void)
         {
             break;
         }
+
+        process_input(&keys, &player);
+
+        // --- Collision Detection ---
+
+        // Stop player from leaving left or right side of the window
+        int player_x_movement = player.x_velocity * game.delta_time;
+        if (player.x + player_x_movement < 0 || (player.x + player.w) + player_x_movement > game.window_width)
+        {
+            player.x_velocity = 0;
+        }
+
+        // Stop player from leaving top or bottom side of the window
+        int player_y_movement = player.y_velocity * game.delta_time;
+        if (player.y + player_y_movement < 0 || (player.y + player.h) + player_y_movement > game.window_height)
+        {
+            player.y_velocity = 0;
+        }
+
+        // --- Update entities ---
+
+        // Update Player POS
+        player.x += player.x_velocity * game.delta_time;
+        player.y += player.y_velocity * game.delta_time;
+
+        // --- Render ---
 
         // Clear previous frame and set background colour
         SDL_SetRenderDrawColor(game.renderer, 96, 128, 255, 255);
@@ -117,7 +158,10 @@ int main(void)
         SDL_RenderPresent(game.renderer);
     }
 
-    // Cleanup
+    // --- Cleanup ---
+
+    SDL_DestroyTexture(player.texture);
+
     SDL_DestroyWindow(game.window);
     SDL_DestroyRenderer(game.renderer);
 
@@ -125,6 +169,37 @@ int main(void)
     SDL_Quit();
 
     return 0;
+}
+
+void process_input(Keys *keys, Player *player)
+{
+    if (keys->up && !keys->down)
+    {
+        player->y_velocity = -player->speed;
+    }
+    else if (keys->down && !keys->up)
+    {
+        player->y_velocity = player->speed;
+    }
+    else
+    {
+        player->y_velocity = 0;
+    }
+
+    if (keys->left && !keys->right)
+    {
+        player->x_velocity = -player->speed;
+    }
+    else if (keys->right && !keys->left)
+    {
+        player->x_velocity = player->speed;
+    }
+    else
+    {
+        player->x_velocity = 0;
+    }
+
+    return;
 }
 
 
@@ -138,6 +213,61 @@ void handle_keyboard(Keys *keys)
         {
             case SDL_QUIT:
                 keys->quit = true;
+                break;
+
+            case SDL_KEYDOWN:
+                if (event.key.repeat == 0)
+                {
+                    switch (event.key.keysym.scancode)
+                    {
+                        case SDL_SCANCODE_W:
+                            keys->up = true;
+                            break;
+                        case SDL_SCANCODE_S:
+                            keys->down = true;
+                            break;
+                        case SDL_SCANCODE_A:
+                            keys->left = true;
+                            break;
+                        case SDL_SCANCODE_D:
+                            keys->right = true;
+                            break;
+                        case SDL_SCANCODE_X:
+                            keys->fire = true;
+                            break;
+                        case SDL_SCANCODE_ESCAPE:
+                            keys->quit = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                break;
+
+            case SDL_KEYUP:
+                if (event.key.repeat == 0)
+                {
+                    switch (event.key.keysym.scancode)
+                    {
+                        case SDL_SCANCODE_W:
+                            keys->up = false;
+                            break;
+                        case SDL_SCANCODE_S:
+                            keys->down = false;
+                            break;
+                        case SDL_SCANCODE_A:
+                            keys->left = false;
+                            break;
+                        case SDL_SCANCODE_D:
+                            keys->right = false;
+                            break;
+                        case SDL_SCANCODE_X:
+                            keys->fire = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
                 break;
 
             default:
