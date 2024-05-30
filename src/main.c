a4afe81d4469202e35b73e59eccf7ade71e7a802
James <jamesmalcolm444@gmail.com>
2024-05-30 19:23:38 +0100

    Game config now using heap

--- src/main.c
+++ src/main.c
@@ -23,6 +23,33 @@
 #include <scores.h>
 #include <vfx.h>
 
+Game *setup_game_config(void)
+{
+    Game *game = malloc(sizeof(Game));
+    if (!game)
+    {
+        printf("[*] Failed to malloc needed memory.\n");
+        return game;
+    }
+
+    game->window_width = 1280;
+    game->window_height = 720;
+
+    game->score = 0;
+    game->score_changed = false;
+    game->highscore = 0;
+    game->delta_time = 0.0f;
+    game->last_frame_time = 0;
+    game->fps = 0;
+    game->frame_counter = 0;
+    game->frame_time = 0.0f;
+
+    game->bg_music = NULL;
+
+    game->reset = false;
+
+    return game;
+}
 
 int main(void)
 {
@@ -30,25 +57,14 @@ int main(void)
     srand(time(NULL));
 
     // Game Config
-    Game game;
+    Game *game = setup_game_config();
+    if (!game)
+    {
+        printf("[*] Exiting...\n");
+        return 1;
+    }
 
-    game.window_width = 1280;
-    game.window_height = 720;
-
-    game.score = 0;
-    game.score_changed = false;
-    game.highscore = 0;
-    game.delta_time = 0.0;
-    game.last_frame_time = 0;
-    game.fps = 0;
-    game.frame_counter = 0;
-    game.frame_time = 0.0;
-
-    game.bg_music = NULL;
-
-    game.reset = false;
-
-    if (init_sdl(&game))
+    if (init_sdl(game))
     {
         printf("[*] Exiting...\n");
         return 1;
@@ -58,41 +74,41 @@ int main(void)
     Keys keys = {false, false, false, false, false, false};
 
     Player player;
-    setup_player(&player, &game);
+    setup_player(&player, game);
 
     EnemyContainer enemy_container;
-    setup_enemies(&enemy_container, &game);
+    setup_enemies(&enemy_container, game);
 
-    load_music(&game, "assets/audio/bg_music.ogg");
+    load_music(game, "assets/audio/bg_music.ogg");
 
     Background background;
-    background.texture = IMG_LoadTexture(game.renderer, "assets/background.png");
+    background.texture = IMG_LoadTexture(game->renderer, "assets/background.png");
     background.x_shift = 0;
 
-    setup_starfield(background.stars, &game);
+    setup_starfield(background.stars, game);
 
     Explosions explosions;
-    explosions.texture = IMG_LoadTexture(game.renderer, "assets/explosion.png");;
+    explosions.texture = IMG_LoadTexture(game->renderer, "assets/explosion.png");;
     SDL_QueryTexture(explosions.texture, NULL, NULL, &explosions.width, &explosions.height);
     explosions.vector = explosion_create_vector();
 
     Hud hud;
-    hud.texture = IMG_LoadTexture(game.renderer, "assets/font.png");
+    hud.texture = IMG_LoadTexture(game->renderer, "assets/font.png");
     SDL_QueryTexture(hud.texture, NULL, NULL, &hud.width, &hud.height);
     sprintf(hud.score, "SCORE: 000");
     sprintf(hud.highscore, "HIGH SCORE: 000");
     sprintf(hud.fps, "FPS: 0");
 
     // Start background music
-    Mix_PlayMusic(game.bg_music, -1);
+    Mix_PlayMusic(game->bg_music, -1);
     Mix_VolumeMusic(32);
 
     // --- Game Loop ---
     while (1)
     {
         // Deltatime for current frame
-        game.delta_time = (SDL_GetTicks() - game.last_frame_time) / 1000.0f;
-        game.last_frame_time = SDL_GetTicks();
+        game->delta_time = (SDL_GetTicks() - game->last_frame_time) / 1000.0f;
+        game->last_frame_time = SDL_GetTicks();
 
         // Respond to users keyboard input
         handle_keyboard(&keys);
@@ -106,64 +122,64 @@ int main(void)
 
         // --- Update Entities ---
 
-        update_player(&player, &game, &enemy_container, explosions.vector);
-        update_enemies(&enemy_container, &game, &player);
+        update_player(&player, game, &enemy_container, explosions.vector);
+        update_enemies(&enemy_container, game, &player);
 
         // Check if game should reset
-        if (game.reset)
+        if (game->reset)
         {
-            game.reset = false;
+            game->reset = false;
 
-            reset_player(&game, &player);
+            reset_player(game, &player);
             reset_enemies(&enemy_container);
             reset_explosions(&explosions);
 
             // Reset score
-            game.score = 0;
+            game->score = 0;
             sprintf(hud.score, "SCORE: 000");
 
             // Present user with highscores
-            trap_highscores_view(&game, &keys, &background, &hud);
+            trap_highscores_view(game, &keys, &background, &hud);
 
             continue;
         }
 
-        update_background(&background, &game);
+        update_background(&background, game);
         update_explosions(&explosions);
 
-        update_hud(&game, &hud);
+        update_hud(game, &hud);
 
         // --- Render ---
 
         // Clear previous frame
-        SDL_RenderClear(game.renderer);
+        SDL_RenderClear(game->renderer);
 
-        render_background(&background, &game);
-        render_explosions(&game, &explosions);
+        render_background(&background, game);
+        render_explosions(game, &explosions);
 
-        render_player(&player, &game);
-        render_enemies(&enemy_container, &game);
+        render_player(&player, game);
+        render_enemies(&enemy_container, game);
 
-        render_hud(&game, &hud);
+        render_hud(game, &hud);
 
         // Flip front and back buffers
-        SDL_RenderPresent(game.renderer);
+        SDL_RenderPresent(game->renderer);
 
         // --- FPS ---
 
-        game.frame_counter++;
-        game.frame_time += game.delta_time;
+        game->frame_counter++;
+        game->frame_time += game->delta_time;
 
         // Second has passed
-        if (game.frame_time > 1.0)
+        if (game->frame_time > 1.0)
         {
-            game.fps = game.frame_counter / game.frame_time;
-            game.frame_counter = 0;
-            game.frame_time = 0;
+            game->fps = game->frame_counter / game->frame_time;
+            game->frame_counter = 0;
+            game->frame_time = 0;
 
-            update_hud_fps(&game, &hud);
+            update_hud_fps(game, &hud);
 
-            // printf("[*] %i FPS\n", game.fps);
+            // printf("[*] %i FPS\n", game->fps);
         }
     }
 
@@ -185,7 +201,7 @@ int main(void)
     SDL_DestroyTexture(hud.texture);
 
     // Free audio
-    Mix_FreeMusic(game.bg_music);
+    Mix_FreeMusic(game->bg_music);
     Mix_FreeChunk(player.sounds[PLAYER_FIRE]);
     Mix_FreeChunk(player.sounds[PLAYER_DIE]);
     Mix_FreeChunk(enemy_container.sounds[ENEMY_FIRE]);
@@ -194,8 +210,10 @@ int main(void)
     Mix_CloseAudio();
 
     // Free window
-    SDL_DestroyWindow(game.window);
-    SDL_DestroyRenderer(game.renderer);
+    SDL_DestroyWindow(game->window);
+    SDL_DestroyRenderer(game->renderer);
+
+    free(game);
 
     // Terminate SDL
     Mix_Quit();
